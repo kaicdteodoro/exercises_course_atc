627ea6ae8993694521182941f0dfc8d7d131d4a3
K41c <kaic.teodoro@mazzafc.tech>
2022-01-31 21:42:26 -0300

    save

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -11,7 +11,11 @@ class Bill extends Model
 
     protected $fillable = ['invoice', 'installment', 'value', 'client_id', 'due_date', 'payment_date'];
     protected $casts = ['invoice' => 'int', 'installment' => 'int', 'value' => 'float', 'client_id' => 'int'];
-    public static $rules = ['invoice' => 'inte|required', 'installment' => 'int|required', 'value' => 'float|required'];
+    public static $rules = [
+        'invoice' => 'int|required',
+        'installment' => 'int|required',
+        'value' => 'float|required'
+    ];
     public static $messages = ['invoice.required' => 'Uma fatura é obrigatória'];
     protected $dates = ['due_date', 'payment_date', 'created_at', 'updated_at'];
 

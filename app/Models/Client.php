0da87b561c58276aec4d9fdcef6db62247700064
K41c <kaic.teodoro@mazzafc.tech>
2022-01-31 21:21:47 -0300

    save

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -13,9 +13,9 @@ class Client extends Model
     public static $form_data = ['name', 'email', 'phone', 'id_number'];
     public static $rules = [
         'name' => 'string|required|max:255',
-        'email' => 'string|required|max:255|unique:users',
-        'phone' => 'string|required|max:255',
-        'id_number' => 'string|required|max:255'
+//        'email' => 'string|required|max:255|unique:users',
+//        'phone' => 'string|required|max:255',
+//        'id_number' => 'string|required|max:255'
     ];
     protected $casts = ['name' => 'string', 'email' => 'string', 'phone' => 'string', 'id_number' => 'string'];
 
